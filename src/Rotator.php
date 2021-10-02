a4be11bbe593e4da9fa65711bee28a0aa2375954
Kanstantsin <haspadar@gmail.com>
2021-10-03 01:14:08 +0300

    added rotator request logging

--- src/Rotator.php
+++ src/Rotator.php
@@ -1,26 +1,39 @@
 <?php
 namespace Pylesos;
 
+use Monolog\Logger;
+
 class Rotator
 {
-    private Request $request;
-
     private array $proxies;
 
-    public function __construct(Request $request)
+    private ?Logger $logger;
+
+    public function __construct(Request $request, ?Logger $logger = null)
     {
-        $this->request = $request;
+        $this->logger = $logger;
         $addresses = Proxies::getAddresses($request);
         $this->proxies = Proxies::generateProxies($addresses);
+        $this->log('New proxy list requested');
     }
 
     public function popProxy(): ?Proxy
     {
-        return $this->proxies ? array_pop($this->proxies) : null;
+        $proxy = $this->proxies ? array_pop($this->proxies) : null;
+        $this->log(sprintf('Proxy list has decreased, there are %d left', count($this->proxies)));
+
+        return $proxy;
     }
 
     public function getProxies(): array
     {
         return $this->proxies;
+    }
+
+    private function log(string $message)
+    {
+        if ($this->logger) {
+            $this->logger->info($message);
+        }
     }
 }
